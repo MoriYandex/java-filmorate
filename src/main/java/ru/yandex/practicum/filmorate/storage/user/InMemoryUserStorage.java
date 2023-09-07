af9fe1d4cb1075c44e2d0fb9f03acf1a123930df
muhamed <zhiletezhevmuhamed-ali@yandex.ru>
2023-09-07 10:31:34 +0300

    fix: Исправить ошибки add-director

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -6,7 +6,10 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
